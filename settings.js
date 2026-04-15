6228d097d3ecbcf1b9db30cd962afde2b5d7eb08
cybermaster04 <macdonaldchiwanika2@gmail.com>
2026-04-15 07:51:52 +0200

    Update and rename Dockerfile to settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'CYBERMASTER-MD',
+  author: 'CYBERMASTER TECH',
+  botName: "CYBERMASTER-MD",
+  botOwner: 'Cybermaster', // Your name
+  ownerNumber: '263781123304', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "Advanced multi-device WhatsApp bot powered by CYBERMASTER TECH for automation and group management.",
+  version: "1.0.0",
+  updateZipUrl: "https://github.com/cybermaster04/CYBERMASTER-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
